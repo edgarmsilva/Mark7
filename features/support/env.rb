93e71ba4d629e0529e35b8547959be6785c95d36
Red <edgar.msilva@gmail.com>
2018-10-15 12:35:15 -0300

    não usa chave no lugar de parenteses!

--- features/support/env.rb
+++ features/support/env.rb
@@ -16,17 +16,17 @@ if @browser.eql?('headless')
     
     args = ['--no-default-browser-check']
 
-    caps = Selenium::WebDriver::Remote::Capabilities.chrome{
-        'chromeOptions' => {'args' => args}   
-    }
+    caps = Selenium::WebDriver::Remote::Capabilities.chrome(
+        'chromeOptions' => {'args' => args}
+    )
     
     Capybara.register_driver: selenium do |app|
-        Capybara::Selenium::Driver.new{
+        Capybara::Selenium::Driver.new(
             app,
             browser: :remote,
             url: 'http://selenium:4444/wd/hub'
             desired_capabilities: caps
-        }
+        )
     end
 else
     puts 'Executando sem headless'
